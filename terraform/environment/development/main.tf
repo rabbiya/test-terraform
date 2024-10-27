779e85a5ee95cbb821efc34bc59a2e3e5eb7b87d
rabbiya <rabbiyaa24@gmail.com>
2024-10-27 22:16:59 +0500

    Update main.tf

--- terraform/environment/development/main.tf
+++ terraform/environment/development/main.tf
@@ -3,21 +3,21 @@ provider "aws" {
 }
 
 module "security_group" {
-  source = "./security_group"
+  source = "../modules/security_group"  # Updated path to the security_group module
 }
 
 module "key_pair" {
-  source        = "./key_pair"
+  source        = "../modules/key_pair"  # Updated path to the key_pair module
   key_name      = var.key_name
   ssh_public_key = var.ssh_public_key
 }
 
 module "ec2" {
-  source          = "./ec2"
+  source          = "../modules/ec2"      # Updated path to the ec2 module
   ami_id         = var.ami_id
   instance_type  = var.instance_type
   instance_name  = var.instance_name
   eip_name       = var.eip_name
   key_name       = module.key_pair.key_name  # Get the key name from the key pair module
-  sg_id         = module.security_group.sg_id  # Get the security group ID from the security group module
+  sg_id          = module.security_group.sg_id  # Get the security group ID from the security group module
 }
