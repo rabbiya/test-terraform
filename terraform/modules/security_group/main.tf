a057db5af04daeb27689a2c7ed626a4b7bd08b86
rabbiya <rabbiyaa24@gmail.com>
2024-10-27 21:33:36 +0500

    Create main.tf

---
+++ terraform/modules/security_group/main.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "mawsim_sg" {
+  name        = "var.sg_name"
+  description = "Allow SSH and HTTP access"
+
+  ingress {
+    from_port   = 22  # Allow SSH
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere (change as needed)
+  }
+
+  ingress {
+    from_port   = 80  # Allow HTTP
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere (change as needed)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
