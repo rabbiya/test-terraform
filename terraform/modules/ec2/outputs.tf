bf84f74b1a912699b5b02eb3ef4e3f9853a40907
rabbiya <rabbiyaa24@gmail.com>
2024-11-03 19:02:12 +0500

    Update outputs.tf

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,4 +1,9 @@
-output "eip_public_ip" {
-  value = aws_eip_association.mawsim_eip_association.public_ip
-  description = "The public IP address of the existing Elastic IP associated with the EC2 instance."
+output "instance_eip" {
+  value       = aws_eip_association.mawsim_eip_association.public_ip  # Associated EIP ka public IP
+  description = "The public IP address of the Elastic IP associated with the EC2 instance."
+}
+
+output "instance_id" {
+  value       = aws_instance.mawsim_dev_server.id  # Instance ka ID
+  description = "The ID of the EC2 instance."
 }
