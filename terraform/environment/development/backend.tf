9581320f5eff44039ff43549cf5fb9c628226fed
rabbiya <rabbiyaa24@gmail.com>
2024-11-03 23:38:50 +0500

    Update backend.tf

--- terraform/environment/development/backend.tf
+++ terraform/environment/development/backend.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "s3" {
-    bucket         = "mawsim-test-dev-terraform-state324"
+    bucket         = "mawsim-test-dev-terraform-state32"
     key            = "terraform/state/terraform.tfstate"
-    region         = "me-central-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
