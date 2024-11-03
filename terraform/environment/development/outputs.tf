3d649386acbdc13d69d45cf6a92297a79e7f44fe
rabbiya <rabbiyaa24@gmail.com>
2024-11-03 19:17:25 +0500

    Update outputs.tf

--- terraform/environment/development/outputs.tf
+++ terraform/environment/development/outputs.tf
@@ -2,6 +2,3 @@ output "security_group_id" {
   value = module.security_group.sg_id  # Security Group ID from the security group module
 }
 
-output "instance_eip" {
-  value = module.ec2.mawsim_dev_eip  # Use the correct output name from the module
-}
