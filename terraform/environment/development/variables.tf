9e8a5d4faeb4cd9b23bc09046d13421b1bac23fd
rabbiya <rabbiyaa24@gmail.com>
2024-11-05 05:46:06 +0500

    Update variables.tf

--- terraform/environment/development/variables.tf
+++ terraform/environment/development/variables.tf
@@ -13,18 +13,8 @@ variable "instance_name" {
   type        = string
 }
 
-variable "eip_name" {
-  description = "Name for the Elastic IP"
-  type        = string
-}
-
 variable "key_name" {
   description = "Name of the key pair to use for SSH access"
-  type        = string
-}
-
-variable "ssh_public_key" {
-  description = "Public SSH key for the key pair"
   type        = string
 }
 
